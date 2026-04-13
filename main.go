7b67581a5ee0217f21ea881d87c21a17b1e5f17a
Ezequiel Bomfim <ezequielcb2014@gmail.com>
2026-04-13 00:30:44 -0300

    Finalizando projeto

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Meu App Go language, no GitOps, com Argo!!"))
+		w.Write([]byte("Meu App Go language, no GitOps, com Argo, finalizado!"))
 	})
 	http.ListenAndServe(":8090", nil)
 }
